acdf46d4f60a0e23de2e2b58cb64ff53eef46c20
martinnomm <nommmartin@gmail.com>
2018-10-27 20:05:06 +0300

    First shet

---
+++ Dices.py
@@ -0,0 +1,20 @@
+from random import randint
+
+def D4():
+    return randint(1,4)
+
+def D6():
+    return randint(1,6)
+
+def D8():
+    return randint(1,8)
+
+def D10():
+    return randint(1,10)
+
+def D12():
+    return randint(1,12)
+
+def D20():
+    return randint(1,20)
+
